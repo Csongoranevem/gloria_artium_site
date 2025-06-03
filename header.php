5acfa2f31bbd37d459a95429146ac6974a23b242
PiciMadar <rekdavid@turr.hu>
2025-06-03 12:17:39 +0200

    Főoldal kártyái, cím, fejléc

--- header.php
+++ header.php
@@ -1,8 +1,26 @@
-<nav class="navbar">
-    <ul class="nav-links">
-        <li><a href="#" class="active">FŐOLDAL</a></li>
-        <li><a href="#">KAPCSOLAT</a></li>
-        <li><a href="#">GYIK</a></li>
-        <li><a href="#">BLOG</a></li>
-    </ul>
-</nav>
+<!DOCTYPE html>
+
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title><?php wp_title(); ?></title>
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+
+<header>
+    <nav class="navbar">
+        <div class="nav-links">
+            <div class="active"><a href="">FőOLDAL</a></div>
+            <div class="inactive"><a href="">KAPCSOLAT</a></div>
+            <div class="inactive"><a href="">GYIK</a></div>
+            <div class="inactive"><a href="">BLOG</a></div>
+        </div>
+    </nav>
+</header>
+
+
+
--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <title>Gloria Artium</title>
     <link rel="stylesheet" href="style.css">
 </head>
+<?php get_header(); ?>
+
 <body>
 
 <head>
@@ -14,6 +16,28 @@
     <style>
         <?php include 'style.css'; ?>
     </style>
-    <h1>GLORIA ARTIUM</h1>
-</main>
-<?php get_footer(); ?>
+    <div class="container">
+        <div class="KozepCim">
+            <h1>GLORIA ARTIUM</h1>
+            <h3 class="TransParent"><i>,,pactum serva"</i></h3>
+        </div>
+        <div class="Kartyak">
+            <div class="Kartya SzelsoKartya">
+                <img src="<?php echo get_template_directory_uri(); ?>/Képek/FestmenyekKep.png" alt="" class="KartyaIMG">
+                <p class="KartyaNAV">FESTMÉNYEK</p>
+            </div>
+            <div class="Kartya" id="SzobraszID">
+                <img src="<?php echo get_template_directory_uri(); ?>/Képek/SzobraszatKep.png" alt="" class="KartyaIMG">
+                <p class="KartyaNAV">SZOBRÁSZAT</p>
+            </div>
+            <div class="Kartya" id="ZeneID">
+                <img src="<?php echo get_template_directory_uri(); ?>/Képek/ZeneKep.png" alt="" class="KartyaIMG">
+                <p class="KartyaNAV">ZENE</p>
+            </div>
+            <div class="Kartya SzelsoKartya">
+                <img src="<?php echo get_template_directory_uri(); ?>/Képek/IrodalomKep.png" alt="" class="KartyaIMG">
+                <p class="KartyaNAV">IRODALOM</p>
+            </div>
+        </div>        
+    </div>
+    </main>
